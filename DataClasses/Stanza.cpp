1e4ae99d45a369a8d8399d65851071c96924ee16
Lopatkin Daniil <daniil199525@gmail.com>
2016-04-11 13:12:32 +0300

    Parsing added.

---
+++ DataClasses/Stanza.cpp
@@ -0,0 +1,91 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include "Stanza.hpp"
+
+void Stanza::load(const std::string &str)
+{
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	read_xml(str, pt);
+		
+	from = pt.get<JID>("Stanza.from");
+	
+	to = pt.get<JID>("Stanza.to");
+
+	type = pt.get<StanzaType>("Stanza.Type");
+
+	switch ( type )
+	{
+		case MESSAGE :
+		{
+			body = pt.get<std::string>("Stanza.Msg.Body");
+			subType = pt.get<subType>("Stanza.Msg.type");
+			break;
+		}
+		case PRESENCE :
+		{
+			subType = pt.get<subType>("Stanza.Presense.Status");
+			break;
+		}
+		case IQ :
+		{
+			subType = pt.get<subType>("Stanza.IQ.type");
+			break;
+		}
+		case ROSTER:
+		{
+			BOOST_FOREACH(ptree::value_type &v,
+            			      pt.get_child("Stanza.Roster.users"))
+			availabilityList.insert(v.second.data());
+			break;
+		}
+		default :
+		{
+			return;
+		}
+	}
+}
+
+
+
+void Stanza::save(const std::string &str)
+{
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	pt.put("Stanza.from", from);
+	pt.put("Stanza.to", to);
+	pt.put("Stanza.Type",type);
+	
+	switch ( type )
+        {
+                case MESSAGE :
+                {
+                        pt.put("Stanza.Msg.Body", body);
+                        pt.put("Stanza.Msg.type", subType);
+                        break;
+                }
+                case PRESENCE :
+                {
+                        pt.put("Stanza.Presense.Status", subType);
+                        break;
+                }
+                case IQ :
+                {
+                        pt.put("Stanza.IQ.type", subType);
+                        break;
+                }
+                case ROSTER:
+                {
+                        BOOST_FOREACH(const std::string &name, availabilityList)
+                        pt.put("Stanza.Roster.users.user", name, true);
+                        break;
+                }
+                default :
+                {
+                        break;
+                }
+        }
+   	write_xml(str, pt);
+}
--- DataClasses/Stanza.hpp
+++ DataClasses/Stanza.hpp
@@ -17,77 +17,37 @@ class Stanza
             MESSAGE = 0,
             PRESENCE,
             IQ,
+	    ROASTER,
             MAX_STANZA_TYPE
         }
         StanzaType type;
+	enum SubType
+	{
+	    CHAT = 0,
+            PRIVATE,
+            AVAILABLE,
+            UNAVAILABLE,
+            GET,
+            SET,
+            RESULT,
+            ERROR,
+            MAX_SUBTYPE
+	}
+	SubType subType;
+	std::string body;
+	std::list<JID> availabilityList;     
     public:
         Stanza( JID from, JID to );
         Stanza( const Stanza &obj );
         ~Stanza();
         void setLang( std::string language );
         stanzaType getStanzaType();
+	SubType getSubType();
         JID from();
         JID to();
-};
-
-class MessageStanza : public Stanza
-{
-    private:
-        std::string body;
-        enum MsgType
-        {
-            CHAT = 0,
-            PRIVATE,
-            MAX_MSG_TYPE
-        }
-        MsgType type;
-    public:
-        MessageStanza( std::string body );
-        MessageStanza( const MessageStanza &obj );
-        ~MessageStanza();
-        void setType( msgType type );
-        msgType getStanzaType();
-};
-
-class PresenceStanza : public Stanza
-{
-    private:
-        enum Status
-        {
-            AVAILABLE = 0,
-            UNAVAILABLE,
-            MAX_STATUS
-        }
-        Status status;
-    public:
-        PresenceStanza( Status status = AVAILABLE );
-        PresenceStanza( const PresenceStanza &obj );
-        ~PresenceStanza();
-        Status getStanzaType();
-};
-
-class IQStanza : public Stanza
-{
-    protected:
-        enum IQType
-        {
-            GET = 0,
-            SET,
-            RESULT,
-            ERROR,
-            MAX_IQType
-        }
-        IQType type;
-    public:
-        IQType getIQType();
-};
-
-class Roster : public IQStanza
-{
-    private:
-        std::list<JID> availabilityList;
-    public:
-        std::list<JID>& getAvailable();
+	void load(const std::string &filename);
+	void save(const std::string &filename);
+	std::list<JID>& getAvailable();
         void addAvailable( JID newJID );
 };
 
